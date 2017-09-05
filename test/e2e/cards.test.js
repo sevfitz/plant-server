e3381a663a50875a22983661b92fcb9526979057
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-09-05 10:03:21 -0700

    Get All and test pass

--- test/e2e/cards.test.js
+++ test/e2e/cards.test.js
@@ -14,15 +14,30 @@ describe('Cards api', () => {
         url: 'Test url'
     };
 
+    let testCard1 = {
+        name: 'Test name1',
+        genus: 'Test genus1',
+        species: 'Test species1',
+        description: 'Test description1',
+        url: 'Test url1'
+    };
+
+    let testCard2 = {
+        name: 'Test name2',
+        genus: 'Test genus2',
+        species: 'Test species2',
+        description: 'Test description2',
+        url: 'Test url2'
+    };
+
     function saveCard (card) {
         return request
             .post('/api/cards')
             .send(card)
             .then(res => {
                 let body = res.body;
-                card.__v = body.__v;
                 card._id = body._id;
-                return body;
+                return card;
             });
     }
 
@@ -41,9 +56,18 @@ describe('Cards api', () => {
             });
     });
 
-
-
-
-
-
+    it('Gets all cards', () => {
+        return Promise.all([
+            saveCard(testCard1),
+            saveCard(testCard2)
+        ])
+            .then(savedCards => {
+                testCard1 = savedCards[0];
+                testCard2 = savedCards[1];
+            })
+            .then(() => request.get('/api/cards'))
+            .then(res => res.body)
+            .then(cards => assert.deepEqual(cards, [testCard,testCard1,testCard2]));
+        
+    });
 });
