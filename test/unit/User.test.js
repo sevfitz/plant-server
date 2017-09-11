76c873c9427b93616f6720bdd641126def807418
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-09-10 21:53:48 -0700

    User unit tests complete

--- test/unit/User.test.js
+++ test/unit/User.test.js
@@ -6,14 +6,20 @@ const expectedValidation = () => { throw new Error('expected validation errors')
 
 describe('User model', () => {
 
-    it('validates a good model', () => {
+    it('new user generates hash and passes validation with required fields', () => {
         const user = new User({
             name: 'test user',
             email: 'test email',
-            hash: 'test hash',
             myCards: []
         });
-        return user.validate();
+        const password = 'abc';
+
+        user.generateHash(password);
+
+        assert.notEqual(user.hash, password);
+
+        assert.isOk(user.comparePassword('abc'));
+        assert.isNotOk(user.comparePassword('bad password'));
     });
 
     describe('validation failures', () => {
@@ -27,6 +33,21 @@ describe('User model', () => {
                         assert.ok(errors.name && errors.email && errors.hash);
                         assert.equal(errors.name.kind && errors.email.kind && errors.hash.kind, 'required');
                     });
+        });
+
+        it('fails validation with incorrect field type', () => {
+            const user = new User({
+                name: {},
+                email: {},
+                password: {},
+                myCards: {}
+            });
+            return user.validate()
+                .then(expectedValidation,
+                    ({ errors }) => {
+                        assert.ok(errors.name);
+                    }
+                );
         });
     });
 });
