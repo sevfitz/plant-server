911bea86a7ac086adffcef534809afc9742fb27e
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-09-10 23:00:09 -0700

    Me routes and tests all pass

--- test/e2e/me.test.js
+++ test/e2e/me.test.js
@@ -30,5 +30,31 @@ describe('Users API', () => {
             });
     });
 
+    it('patches user\'s own account info', () => {
+        return request.patch('/api/me')
+            .set('Authorization', token)
+            .send({ email: 'something different' })
+            .then(() => request
+                .get('/api/me')
+                .set('Authorization', token)
+            )
+            .then(res => {
+                me.email = 'something different';
+                const newMe = res.body;
+                assert.deepEqual(newMe, me);
+            });
+    });
+
+    it('deletes user\'s own account', () => {
+        return request.delete('/api/me')
+            .set('Authorization', token)
+            .then(res => assert.deepEqual(res.body, { removed: true }));
+    });
+
+    it('can\'t delete self if not there', () => {
+        return request.delete('/api/me')
+            .set('Authorization', token)
+            .then(res => assert.deepEqual(res.body, { removed: false }));
+    });
 });
 
